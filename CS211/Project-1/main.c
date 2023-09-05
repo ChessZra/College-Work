1f5f14420b68adf4bdcc0d0a3769f56827b59ef3
ChessZra <johnezrasee100@gmail.com>
2023-09-05 03:27:08 -0500

    211

--- CS211/Project-1/main.c
+++ CS211/Project-1/main.c
@@ -12,17 +12,17 @@ Author: John Ezra See
 const int WORLD_SIZE = 65;
 
 typedef struct cell_struct {
-    bool state[3]; //active status for [left, me, right] cells 
-    bool active;   //current active status for this cell
+    bool state[3]; // Active status for [left, me, right] cells 
+    bool active;   // Current active status for this cell
 } cell;
 
-//convert an 8-bit integer rule (0-255) to array of bits 
-//(stored in reverse order)
-//   ex: rule = 6  = 00000110 -> [01100000] 
-//   ex: rule = 30 = 00011110 -> [01111000] 
-//   ex: rule = 65 = 01000001 -> [10000010]
-//return true if input rule is valid (0-255)
-//return false if input rule is invalid
+// Convert an 8-bit integer rule (0-255) to array of bits 
+//  (stored in reverse order)
+//  ex: rule = 6  = 00000110 -> [01100000] 
+//  ex: rule = 30 = 00011110 -> [01111000] 
+//  ex: rule = 65 = 01000001 -> [10000010]
+// Return true if input rule is valid (0-255)
+// Return false if input rule is invalid 
 bool setBitArray(bool bitArray[8], int rule) {
         // Case - invalid rule input:
         if (rule < 0x0 || rule > 0xFF) return false;
@@ -35,13 +35,12 @@ bool setBitArray(bool bitArray[8], int rule) {
         return true;
 }
 
-//convert a 3-bit state array to its 
-//associated index of the rule# bit array
-//   ex: {0 0 0} -> 0
-//   ex: {0 0 1} -> 1
-//   ex: {1 0 1} -> 5
+// Convert a 3-bit state array to its 
+// associated index of the rule# bit array
+// ex: {0 0 0} -> 0
+// ex: {0 0 1} -> 1
+// ex: {1 0 1} -> 5 
 int stateToIndex(bool state[3]) {
-    // Since there are only three bits, for-loop is not necessary.
     int ret = 0;
     if (state[0]) ret += 4;
     if (state[1]) ret += 2;
@@ -49,13 +48,13 @@ int stateToIndex(bool state[3]) {
     return ret;
 }
 
-//update the state array for each cell in the world array based on the
-//current status of active for the nearby [left,me,right] cells
-//note: world is assumed periodic/cyclic,
-//with front-to-back and back-to-front wrapping 
+// Update the state array for each cell in the world array based on the
+// Current status of active for the nearby [left,me,right] cells
+// Note: world is assumed periodic/cyclic,
+//       with front-to-back and back-to-front wrapping 
 void setStates(cell world[WORLD_SIZE]) {
-    // For every cell, 
-    // check the activity of its left and right neighbors (w/ wrapping)
+    //  For every cell, 
+    //  check the activity of its left and right neighbors (w/ wrapping)
     for (int i = 0; i < WORLD_SIZE; i++) {
         int left = i - 1, right = i + 1;
         if (left == -1) left = WORLD_SIZE - 1;
@@ -67,10 +66,10 @@ void setStates(cell world[WORLD_SIZE]) {
     }
 }
 
-//evolve each cell's active status to the next generation 
-//  using its state array
-//ruleBitArray contains the 8-bits for the rule#, 
-//  stored in reverse order
+// Evolve each cell's active status to the next generation 
+// using its state array
+// RuleBitArray contains the 8-bits for the rule#, 
+// stored in reverse order 
 void evolveWorld(cell world[WORLD_SIZE], bool ruleBitArray[8]) {
     // This loop will create the new generation world
     cell newWorld[WORLD_SIZE];
@@ -86,8 +85,8 @@ void evolveWorld(cell world[WORLD_SIZE], bool ruleBitArray[8]) {
     }
 }
 
-// This function will keep on asking for a rule number
-// that satisfies [0, 255] - modifies the parameter
+//  This function will keep on asking for a rule number
+//  that satisfies [0, 255] - modifies the parameter 
 void readValidRule(int* ruleRef) {
     int rule = -1;
     while (rule < 0 || rule > 255) {
@@ -97,8 +96,8 @@ void readValidRule(int* ruleRef) {
     *ruleRef = rule;
 }
 
-// This prints the associated bit array for the read rule number
-// For example, "The bit array for rule #30 is 00011110"
+//  This prints the associated bit array for the read rule number
+//  For example, "The bit array for rule #30 is 00011110" 
 void printBitArray(bool bitArray[8], int rule) {
     printf("\nThe bit array for rule #%d is ", rule);
     for (int i = 7; i >= 0; i--) {
@@ -115,6 +114,9 @@ void printPossibleStates(bool bitArray[8]) {
     }
 }
 
+// The main function is the entry point or the driver
+// code for this program. 
+// It will ask the user for two integers
 int main() {
     cell world[WORLD_SIZE];
     bool bitArray[8];
