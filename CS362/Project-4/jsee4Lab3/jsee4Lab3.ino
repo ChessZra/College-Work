bbd62ad6bcce8ca76c3770462b070a6790c1e4ce
ChessZra <johnezrasee100@gmail.com>
2024-02-11 02:18:57 -0600

    Project 4 : : 02/12/2024

---
+++ CS362/Project-4/jsee4Lab3/jsee4Lab3.ino
@@ -0,0 +1,54 @@
+/* jsee4Lab4.ino */
+/* -----------------------------------------------------------------------------------
+ >> Assignment details and provided code are created and owned by Patrick Troy.
+ >> University of Illinois Chicago - CS 362, Spring 2024
+ >> -----------------------------------------------------------------------------------
+ >> Course : Computer Design
+ >> Author : John Ezra See (668942698, jsee4)
+ >> System : Windows 11 w/ Arduino IDE
+ >> References: https://docs.arduino.cc/ 
+ >>             https://www.circuitbasics.com/how-to-use-photoresistors-to-detect-light-on-an-arduino/
+ - -                             - -
+ >> Project/Lab Name    : Lab 4: Analog Input - Photoresistor
+ >> Brief Description   : The arduino consists of a 16x2 LCD and a photoresistor. 
+ >>                       it will display the time and different levels of brightness.
+ >> Assumptions         : None
+ >> Date demonstrated   : 02/12/2024
+ >> Teaching Assistant  : 
+------------------------------------------------------------------------------------- */
+// include the library code:
+#include <LiquidCrystal.h>
+// Global variables:
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+int photoPin = A0;
+int light;
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // When no light, resistance increases, voltage is low. 
+  // When light, resistance decreases, voltage is high.
+  light = analogRead(photoPin);
+  // dark, partially dark, medium, fully lit, brightly lit 
+  lcd.setCursor(0, 0);
+  if (light <= 5) {
+    lcd.print("dark                     ");
+  } else if (light <= 15) {
+    lcd.print("partially dark           ");
+  } else if (light <= 25) {
+    lcd.print("medium                   ");
+  } else if (light <= 55) {
+    lcd.print("fully lit                ");
+  } else {
+    lcd.print("brightly lit             ");
+  }
+
+  // Serial.println(light);
+  // Display the time:
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+}
