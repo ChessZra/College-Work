8f7c0e7302e101d8f12e4f65ee31b87c07db71b5
ChessZra <johnezrasee100@gmail.com>
2024-04-03 01:00:27 -0500

    Project 4 : : 04/03/24

--- CS251/Project-4/prqueue.h
+++ CS251/Project-4/prqueue.h
@@ -24,13 +24,19 @@ class prqueue {
     NODE* curr;
     NODE* temp;  // Optional
 
-    // TODO_STUDENT: add private helper function definitions here
+    // TODO_STUDENT: add private helper function definitions here ::
+    // This is a helper function for as_string.
+    // This is an in-order traversal, so it is the same as dequeuing order.
     void asStringHelper (NODE* cur, ostream& output) const {
         if (cur == nullptr) return;
 
         this->asStringHelper(cur->left, output);
 
-        output << cur->priority << " value: " << cur->value << endl;
+        NODE* temp = cur;
+        while (temp) {
+            output << temp->priority << " value: " << temp->value << endl;
+            temp = temp->link;
+        }
 
         this->asStringHelper(cur->right, output); 
     }
@@ -44,7 +50,12 @@ class prqueue {
         this->freeAllNodes(cur->left);
         this->freeAllNodes(cur->right);
 
-        delete cur;
+        // For every node, treat it as a linked list and delete them iteratively:
+        while (cur) {
+            NODE* temp = cur->link;
+            delete cur;
+            cur = temp;
+        }
     }
 
     // Helper function for enqueue():
@@ -54,7 +65,7 @@ class prqueue {
         // Iterate while you haven't inserted the node.
         while (true) {
             // If the new node is less, go left:
-            if (newNode->priority <= cur->priority) {
+            if (newNode->priority < cur->priority) {
                 if (cur->left == nullptr) {
                     newNode->parent = cur;
                     cur->left = newNode;
@@ -69,8 +80,69 @@ class prqueue {
                     break;
                 }
                 cur = cur->right;
+            } else { // There is a duplicate in the current head:
+                while (cur->link != nullptr) cur = cur->link;
+                cur->link = newNode;
+
+                break;
             }
         }
+    }
+
+    NODE* getCopy(NODE* root) {
+        if (root == nullptr) return nullptr;
+        NODE* newNode = new NODE;
+        newNode->priority = root->priority;
+        newNode->value = root->value;
+        newNode->parent = nullptr;
+        newNode->link = nullptr;
+
+        NODE* cur = root->link;
+        NODE* newLL = newNode;
+        while (cur) {
+            NODE* temp = new NODE;
+            temp->priority = cur->priority;
+            temp->value = cur->value; 
+            temp->parent = nullptr;
+            temp->link = nullptr;
+            newLL->link = temp;
+            newLL = newLL->link;
+            cur = cur->link;
+        }
+
+        newNode->left = this->getCopy(root->left);
+        newNode->right = this->getCopy(root->right);
+    
+        if (newNode->left != nullptr) newNode->left->parent = newNode;
+        if (newNode->right != nullptr) newNode->right->parent = newNode;
+        return newNode;
+    }
+
+    void sameTree(NODE* root1, NODE* root2, bool& ret) const {
+        if ((!root1 && !root2) || ret == false) return;
+        if ((root1 && !root2) || (!root1 && root2)) {
+            ret = false;
+            return;
+        }
+        // by this line, they are both valid nodes:
+        // now check if they are the same linked list...
+        NODE* l1 = root1;
+        NODE* l2 = root2;
+        while (l1 || l2) {
+            if ((l1 && !l2) || (!l1 && l2)) {
+                ret = false;
+                break;
+            }
+            if (l1->priority != l2->priority || l1->value != l2->value) {
+                ret = false;
+                break;
+            }
+            l1 = l1->link;
+            l2 = l2->link;
+        }
+
+        sameTree(root1->left, root2->left, ret);
+        sameTree(root1->right, root2->right, ret);
     }
 
    public:
@@ -89,7 +161,13 @@ class prqueue {
     ///
     /// Runs in O(N), where N is the number of values in `other`.
     prqueue(const prqueue& other) {
-        // TODO_STUDENT
+        // Important step: assign the values before calling the assignment operator!
+        // This function gets called and it will have only garbage values, the pointer might be invalid..
+    
+        this->sz = 0;
+        this->root = nullptr;
+
+        *this = other;
     }
 
     /// Assignment operator; `operator=`.
@@ -100,7 +178,18 @@ class prqueue {
     /// Runs in O(N + O), where N is the number of values in `this`, and O is
     /// the number of values in `other`.
     prqueue& operator=(const prqueue& other) {
-        // TODO_STUDENT
+        if (this == &other) return *this;
+
+        this->clear();
+        if (other.root == nullptr) {
+            this->root = nullptr;
+            this->sz = 0;
+        } else {
+            this->sz = other.size();
+            this->root = this->getCopy(other.root);
+        }
+
+        return *this;
     }
 
     /// Empties the `prqueue`, freeing all memory it controls.
@@ -133,6 +222,7 @@ class prqueue {
         newNode->left = nullptr;
         newNode->right = nullptr;
         newNode->parent = nullptr;
+        newNode->link = nullptr;
 
         if (this->sz == 0) {
             newNode->parent = nullptr;
@@ -171,22 +261,48 @@ class prqueue {
     T dequeue() {
         if (this->sz == 0) return T{};
         NODE* cur = root;
+        // Get the left most node, which is essentially the node with the least 
+        // priority (the node to dequeue):
         while (cur->left) {
             cur = cur->left;
         }
+        // -- cur is now the node to dequeue in this line --
 
-        if (cur == this->root) {
-            if (cur->right) {
+        if (cur == this->root) { // Case 1: cur (the node to be removed) is the root
+            if (cur->link) {                    // Case 1: cur is a linked list
+                // There is no parent:
+                this->root = cur->link;
+                cur->link->parent = nullptr;
+
+                // Establish link and cur's right child relationship:
+                cur->link->right = cur->right;
+                if (cur->right)
+                    cur->right->parent = cur->link;
+            } else if (cur->right) {            // Case 2: cur has right children
                 this->root = cur->right;
                 cur->right->parent = nullptr;
-            } else {
+            } else {                            // Case 3: cur has no children
                 this->root = nullptr;
             }
-        } else {
-            cur->parent->left = cur->right; // Set its predecessor's child to cur->right.
-            if (cur->right)
-                cur->right->parent = cur->parent; // Set cur->right's parent to predecessor.
+        } else { // Case 2: cur is not the root
+            if (cur->link) {    // Case 1: cur is a linked list
+
+                // Establish link and cur's parent relationship:
+                cur->parent->left = cur->link; 
+                cur->link->parent = cur->parent;
+
+                // Establish link and cur's right child relationship:
+                cur->link->right = cur->right;
+                if (cur->right)
+                    cur->right->parent = cur->link; // Set cur->right's parent to predecessor:
+
+            } else {            // Case 2: cur is not the linked list
+                cur->parent->left = cur->right; // Set its predecessor's child to cur->right:
+                if (cur->right)
+                    cur->right->parent = cur->parent; // Set cur->right's parent to predecessor:
+            }
         }
+
         T ret = cur->value;
         delete cur;
 
@@ -205,9 +321,9 @@ class prqueue {
     ///
     /// See `next` for usage details.
     ///
-    /// O(H), where H is the maximum height of the tree.
+    /// O(1), simply sets the curr to nullptr, to get ready for the first next() call.
     void begin() {
-        // TODO_STUDENT
+        this->curr = nullptr;
     }
 
     /// Uses the internal state to return the next in-order value and priority
@@ -229,7 +345,53 @@ class prqueue {
     /// H is the height of the tree, and M is the number of duplicate
     /// priorities.
     bool next(T& value, int& priority) {
-        // TODO_STUDENT
+        // Base case: the first call to this function is always the minimum value.
+
+        if (curr == nullptr) {
+            if (this->sz == 0) return false;
+            curr = root;
+            while (curr->left) curr = curr->left;
+            value = curr->value;
+            priority = curr->priority;
+            return true;
+        }
+        if (curr->link) { // the next link will be the next node..
+            // Per the instruction, "The other pointers for these remaining nodes are not checked. 
+                                    // This is a design decision for you to make!" 
+            // Our original design:
+            curr->link->right = curr->right; 
+            curr->link->parent = curr->parent;
+
+            curr = curr->link;
+            value = curr->value;
+            priority = curr->priority;
+            return true;
+        } 
+
+        if (curr->right != nullptr) { // If the right subtree exists:
+            NODE* newNode = curr->right;
+            while (newNode->left) newNode = newNode->left;
+
+            curr = newNode;
+            value = newNode->value;
+            priority = newNode->priority;
+        } else { // If not, then we gotta go back up to the parent
+            temp = curr;
+            while (temp->parent && temp->parent->right && temp->parent->right->priority == temp->priority) { 
+                // While the current node is a right child:
+                temp = temp->parent;
+            }
+            temp = temp->parent;
+            curr = temp;
+
+            // We reached the end here!
+            if (!curr) return false;
+
+            value = curr->value;
+            priority = curr->priority;
+        }
+
+        return true; 
     }
 
     /// Converts the `prqueue` to a string representation, with the values
@@ -303,7 +465,11 @@ class prqueue {
     /// either `prqueue`.
     ///
     bool operator==(const prqueue& other) const {
-        // TODO_STUDENT
+        if (other.sz != this->sz) return false;
+        bool ret = true;
+        NODE* root1 = this->root;
+        sameTree(root1, other.root, ret);
+        return ret;
     }
 
     /// Returns a pointer to the root node of the BST.
--- CS251/Project-4/prqueue_tests.cpp
+++ CS251/Project-4/prqueue_tests.cpp
@@ -2,6 +2,7 @@
 
 #include "gtest/gtest.h"
 #include "map"
+#include <vector>
 using namespace std;
 
 TEST(EMPTYTEST, Test) {
@@ -29,7 +30,6 @@ TEST(SIZE, Size) {
     EXPECT_EQ(1, singlePQ.size());
     EXPECT_EQ(6, names.size());
 }
-
 
 TEST(ENQUEUE, Enqueue) {
     // One element in PQ:
@@ -220,3 +220,342 @@ TEST(PEEK, Peek) {
     EXPECT_EQ(1, singlePQ.size());
     EXPECT_EQ(6, names.size());
 }
+
+TEST(DUPLICATES, DuplicateEnqueueAsString) {
+    // One linked list in PQ:
+    prqueue<string> singlePQ;
+    singlePQ.enqueue("LonelyGuy", 5);
+    singlePQ.enqueue("LonelySuccessor", 5);
+
+    // Multiple elements in PQ:
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+
+    string expectedOutput =
+    "2 value: Ford\n"
+    "4 value: Bernard\n"
+    "5 value: Dolores\n"
+    "6 value: Jasmine\n"
+    "8 value: Arnold\n"
+    "8 value: William\n"
+    "8 value: Teddy\n"
+    "11 value: Carlos\n";
+    EXPECT_EQ(expectedOutput, names.as_string());
+
+    expectedOutput =
+    "5 value: LonelyGuy\n"
+    "5 value: LonelySuccessor\n";
+    EXPECT_EQ(expectedOutput, singlePQ.as_string());
+}
+
+TEST(DUPLICATES, DuplicateDequeue) {
+    // One linked list in PQ:
+    prqueue<string> singlePQ;
+    singlePQ.enqueue("LonelyGuy", 5);
+    singlePQ.enqueue("LonelySuccessor", 5);
+
+    vector<string> expectedOutput = {"LonelyGuy", "LonelySuccessor"};
+    for (size_t i = 0; i < expectedOutput.size(); i++) {
+        EXPECT_EQ(expectedOutput[i], singlePQ.dequeue());
+    }
+
+    // Multiple elements in PQ:
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+
+    vector<string> expectedOutput2 = {"Ford", "Bernard", "Dolores", "Jasmine", "Arnold", "William", "Teddy", "Carlos"};
+    for (size_t i = 0; i < expectedOutput2.size(); i++) {
+        EXPECT_EQ(expectedOutput2[i], names.dequeue());
+    }
+}
+
+TEST(COPY, OneItemCopyAssignmentOperator) {
+    // One element in PQ:
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    prqueue<string> clone;
+    clone = names;
+
+    string expectedOutput =
+    "5 value: Dolores\n";
+    EXPECT_EQ(expectedOutput, clone.as_string());
+}
+
+TEST(COPY, EmptyAssignmentOperator) {
+    // Empty elements in PQ:
+    prqueue<string> names;
+    prqueue<string> clone;
+    clone = names;
+    EXPECT_EQ(nullptr, clone.getRoot());
+}
+
+TEST(COPY, MultipleElementsAssignmentOperator) {
+    // Multiple elements in PQ:
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+
+    prqueue<string> clone;
+    clone = names;
+
+    string expectedOutput =
+    "2 value: Ford\n"
+    "4 value: Bernard\n"
+    "5 value: Dolores\n"
+    "6 value: Jasmine\n"
+    "8 value: Arnold\n"
+    "8 value: William\n"
+    "8 value: Teddy\n"
+    "11 value: Carlos\n";
+    EXPECT_EQ(expectedOutput, clone.as_string());
+    EXPECT_EQ(names.size(), clone.size());
+
+    prqueue<string> clone2;
+    clone2.enqueue("A", 11);
+    clone2.enqueue("B", 8);
+    clone2.enqueue("C", 8);
+
+    clone2 = names;
+    EXPECT_EQ(expectedOutput, clone2.as_string());
+    EXPECT_EQ(names.size(), clone2.size());
+}
+
+TEST(COPY, SelfAssignmentOperator) {
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+
+    names = names;
+    string expectedOutput =
+    "2 value: Ford\n"
+    "4 value: Bernard\n"
+    "5 value: Dolores\n"
+    "6 value: Jasmine\n"
+    "8 value: Arnold\n"
+    "8 value: William\n"
+    "8 value: Teddy\n"
+    "11 value: Carlos\n";
+    EXPECT_EQ(expectedOutput, names.as_string());
+}
+
+TEST(COPY, CopyConstructor) {
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+
+    prqueue<string> clone(names);
+    string expectedOutput =
+    "2 value: Ford\n"
+    "4 value: Bernard\n"
+    "5 value: Dolores\n"
+    "6 value: Jasmine\n"
+    "8 value: Arnold\n"
+    "8 value: William\n"
+    "8 value: Teddy\n"
+    "11 value: Carlos\n";
+    EXPECT_EQ(expectedOutput, clone.as_string());
+}
+
+TEST(OPERATOREQUALEQUAL, SameTree) {
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+
+    prqueue<string> names2;
+    names2.enqueue("Dolores", 5);
+    names2.enqueue("Bernard", 4);
+    names2.enqueue("Arnold", 8);
+    names2.enqueue("Ford", 2);
+    names2.enqueue("Jasmine", 6);
+    names2.enqueue("Carlos", 11);
+    names2.enqueue("William", 8);
+    names2.enqueue("Teddy", 8);
+
+    prqueue<string> names3;
+    names3.enqueue("Dolores", 11);
+    names3.enqueue("Arnold", 9);
+    names3.enqueue("Carlos", 22);
+    names3.enqueue("Ford", 2);
+    
+    prqueue<string> emptyPQ;
+    prqueue<string> emptyPQ2;
+
+    prqueue<string> pq;
+    pq.enqueue("10", 10);
+    pq.enqueue("5", 5);
+    pq.enqueue("30", 30);
+    pq.enqueue("40", 40);
+    pq.enqueue("-2", -2);
+    pq.enqueue("6", 6);
+    pq.enqueue("8", 8);
+    pq.enqueue("2", 2);
+    pq.enqueue("-1", -1);
+
+    prqueue<string> pq2;
+    pq2.enqueue("10", 10);
+    pq2.enqueue("5", 5);
+    pq2.enqueue("30", 30);
+    pq2.enqueue("40", 40);
+    pq2.enqueue("-2", -2);
+    pq2.enqueue("6", 6);
+    pq2.enqueue("8", 8);
+    pq2.enqueue("2", 2);
+    pq2.enqueue("-1", 0);
+
+    prqueue<string> pq3;
+    pq3.enqueue("10", 10);
+    pq3.enqueue("5", 5);
+    pq3.enqueue("30", 30);
+    pq3.enqueue("40", 40);
+    pq3.enqueue("-2", -2);
+    pq3.enqueue("6", 6);
+    pq3.enqueue("8", 8);
+    pq3.enqueue("2", 2);
+    pq3.enqueue("1", 0);
+
+    EXPECT_EQ(true, names == names2);
+    EXPECT_EQ(false, names == names3);
+    EXPECT_EQ(true, names == names);
+    EXPECT_EQ(false, emptyPQ == names);
+    EXPECT_EQ(true, emptyPQ == emptyPQ2);
+    EXPECT_EQ(false, pq == names3);
+    EXPECT_EQ(true, pq == pq);
+    EXPECT_EQ(false, pq == pq2);
+    EXPECT_EQ(false, pq3 == pq2);
+}
+
+TEST(BEGINNEXT, BeginNextNoDuplicates) {
+    int i = 0;
+    string value; int priority;
+
+    // No duplicates:
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    vector<string> expectedValue = {"Ford", "Bernard", "Dolores", "Jasmine", "Arnold", "Carlos"};
+    vector<int> expectedPriority = {2, 4, 5, 6, 8, 11};
+    names.begin();
+    i = 0;
+    while (names.next(value, priority)) {
+        EXPECT_EQ(expectedPriority[i], priority);
+        EXPECT_EQ(expectedValue[i], value);  
+        i += 1;
+    }
+    EXPECT_EQ(expectedValue.size(), i);
+
+    // A different priority queue! 10, 5, 30, 40, -2, 6, 8, 2, -1
+    prqueue<string> pq;
+    pq.enqueue("10", 10);
+    pq.enqueue("5", 5);
+    pq.enqueue("30", 30);
+    pq.enqueue("40", 40);
+    pq.enqueue("-2", -2);
+    pq.enqueue("6", 6);
+    pq.enqueue("8", 8);
+    pq.enqueue("2", 2);
+    pq.enqueue("-1", -1);
+    vector<string> expectedValue2 = {"-2", "-1", "2", "5", "6", "8", "10", "30", "40"};
+    vector<int> expectedPriority2 = {-2, -1, 2, 5, 6, 8, 10, 30, 40};
+    pq.begin();
+    i = 0;
+    while (pq.next(value, priority)) {
+        EXPECT_EQ(expectedPriority2[i], priority);
+        EXPECT_EQ(expectedValue2[i], value);  
+        i += 1;
+    }
+    EXPECT_EQ(expectedValue2.size(), i);
+
+    prqueue<string> emptyPQ;
+    emptyPQ.begin();
+    emptyPQ.next(value, priority);
+}
+
+TEST(NEXT, NextDuplicatesEnd) {
+    string value; int priority;
+    int i = 0;
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+
+    // Now handle duplicates!
+    vector<string> expectedValue2 = {"Ford", "Bernard", "Dolores", "Jasmine", "Arnold", "William", "Teddy"};
+    vector<int> expectedPriority2 = {2, 4, 5, 6, 8, 8, 8};
+    names.begin();
+    while(names.next(value, priority)) {
+        EXPECT_EQ(expectedValue2[i], value);
+        EXPECT_EQ(expectedPriority2[i], priority);
+        i++;
+    }
+    EXPECT_EQ(expectedValue2.size(), i);
+}
+
+TEST(NEXT, MoreDuplicates) {
+    string value; int priority;
+    int i = 0;
+    vector<string> expectedValue = {"Ford", "Bernard", "Dolores", "Jasmine", "Arnold", "William", "Teddy", "Carlos"};
+    vector<int> expectedPriority = {2, 4, 5, 6, 8, 8, 8, 11};
+    prqueue<string> names;
+    names.enqueue("Dolores", 5);
+    names.enqueue("Bernard", 4);
+    names.enqueue("Arnold", 8);
+    names.enqueue("Ford", 2);
+    names.enqueue("Jasmine", 6);
+    names.enqueue("Carlos", 11);
+    names.enqueue("William", 8);
+    names.enqueue("Teddy", 8);
+    names.begin();
+    while (names.next(value, priority)) {
+        EXPECT_EQ(expectedValue[i], value);
+        EXPECT_EQ(expectedPriority[i], priority);
+        i++;
+    }
+    EXPECT_EQ(expectedValue.size(), i);
+}
+
