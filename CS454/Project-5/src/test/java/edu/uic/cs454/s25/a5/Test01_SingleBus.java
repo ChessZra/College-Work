5780fafac009b7d68d6cb14e4f7d388a21a7443f
ChessZra <johnezrasee100@gmail.com>
2025-04-26 15:48:17 -0500

    Project 5 : : 04/26/25

--- CS454/Project-5/src/test/java/edu/uic/cs454/s25/a5/Test01_SingleBus.java
+++ CS454/Project-5/src/test/java/edu/uic/cs454/s25/a5/Test01_SingleBus.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Test01_SingleBus {
+    
     @Test
     public void testBusIdentity() {
         Depot depot = Depot.createDepot();
