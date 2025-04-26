9ef3a5ae34ea1ebb80258a6968624c56b677cd33
ChessZra <johnezrasee100@gmail.com>
2025-04-26 15:46:07 -0500

    Project 0 : : 01/25/25

--- CS454/Project-0/src/main/java/edu/uic/cs454/a0/Assignment0.java
+++ CS454/Project-0/src/main/java/edu/uic/cs454/a0/Assignment0.java
@@ -3,7 +3,7 @@ package edu.uic.cs454.a0;
 public class Assignment0 {
   public static void main(String[] args) {
     String yourKey = "b46acbf482360995388962eaa223ef06"; // Please replace the contents of this string with your key from Travis
-
     System.exit((args.length < 1 || !args[0].equals(yourKey)) ? 1 : 0);
   }
 }
+
